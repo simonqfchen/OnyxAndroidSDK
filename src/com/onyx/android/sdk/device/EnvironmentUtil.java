7877dec82e815355658b89ef121ea3bbabd0dbac
joy <joy@onyx-international.com>
2013-03-21 21:38:08 +0800

    add EnvironmentUtil.getDeviceSerial() method

--- src/com/onyx/android/sdk/device/EnvironmentUtil.java
+++ src/com/onyx/android/sdk/device/EnvironmentUtil.java
@@ -4,6 +4,13 @@
 package com.onyx.android.sdk.device;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+import android.content.Context;
+import android.provider.Settings.Secure;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 /**
  * duplication of android.os.Environment to provide device specific function
@@ -13,6 +20,8 @@ import java.io.File;
  */
 public class EnvironmentUtil
 {
+    private static final String TAG = "EnvironmentUtil";
+    
     public static final File EXTERNAL_STORAGE_ANDROID_DATA_DIRECTORY = new File(new File(getExternalStorageDirectory(),
             "Android"), "data");
     
@@ -72,5 +81,33 @@ public class EnvironmentUtil
         return DeviceInfo.singleton().getDeviceController().isFileOnRemovableSDCard(file);
     }
     
-    
+    /**
+     * never return null;
+     * 
+     * @param context
+     * @return
+     */
+    public static String getDeviceSerial(Context context)
+    {
+        UUID uuid = null;
+
+        final String androidId = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
+
+        // Use the Android ID unless it's broken, in which case fallback on deviceId,
+        // unless it's not available, then fallback on a random number which we store
+        // to a prefs file
+        try {
+            if (!"9774d56d682e549c".equals(androidId)) {
+                uuid = UUID.nameUUIDFromBytes(androidId.getBytes("utf8"));
+            } else {
+                final String deviceId = ((TelephonyManager) context.getSystemService( Context.TELEPHONY_SERVICE )).getDeviceId();
+                uuid = deviceId!=null ? UUID.nameUUIDFromBytes(deviceId.getBytes("utf8")) : UUID.randomUUID();
+            }
+        } catch (UnsupportedEncodingException e) {
+            Log.w(TAG, "exception", e);
+            uuid = UUID.randomUUID();
+        }
+        
+        return uuid.toString();
+    }
 }
