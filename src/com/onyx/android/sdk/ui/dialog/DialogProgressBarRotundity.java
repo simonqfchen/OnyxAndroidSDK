021d56ce40af038e283c076f5249e9cb570c2a15
joy <joy@onyx-international.com>
2013-04-29 11:15:39 +0800

    use OnCancelListener directly instead of onDismissDialogListener

--- src/com/onyx/android/sdk/ui/dialog/DialogProgressBarRotundity.java
+++ src/com/onyx/android/sdk/ui/dialog/DialogProgressBarRotundity.java
@@ -15,54 +15,20 @@ import com.onyx.android.sdk.R;
  */
 public class DialogProgressBarRotundity extends Dialog
 {
-    public interface onDismissDialogListener
-    {
-        public void dismissdialog();
-    }
-    private onDismissDialogListener mOnDismissDialogListener = new onDismissDialogListener()
-    {
-        
-        @Override
-        public void dismissdialog()
-        {
-            //do nothing
-        }
-    };
-    public void setOnDismissdialogListener(onDismissDialogListener l)
-    {
-        mOnDismissDialogListener = l;
-    }
-    
-    private boolean mIsDimissedByUser = false;
-
     public DialogProgressBarRotundity(Context context)
     {
         super(context, R.style.dialog_progress_style);
 
         setContentView(R.layout.dialog_progressbar_rotundity);
     }
-    
-    public boolean isDismissedByUser()
-    {
-        return mIsDimissedByUser;
-    }
 
     @Override
         public boolean onKeyDown(int keyCode, KeyEvent event)
     {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            mIsDimissedByUser = true;
-            this.dismiss();
+            this.cancel();
             return true;
         }
         return super.onKeyDown(keyCode, event);
-    }
-
-
-    @Override
-    public void dismiss()
-    {
-        super.dismiss();
-        mOnDismissDialogListener.dismissdialog();
     }
 }
