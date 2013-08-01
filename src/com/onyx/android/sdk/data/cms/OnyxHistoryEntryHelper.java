9ed0bfe015d52fff43bdf826e589d3f37cc1eae7
onyx-peekaboo <peekaboo@onyx-international.com>
2013-08-01 18:04:15 +0800

    OnyxAndroidSDK:Save Page Information

--- src/com/onyx/android/sdk/data/cms/OnyxHistoryEntryHelper.java
+++ src/com/onyx/android/sdk/data/cms/OnyxHistoryEntryHelper.java
@@ -19,19 +19,21 @@ public class OnyxHistoryEntryHelper
 	
 	private static OnyxHistoryEntry sHistoryEntry = new OnyxHistoryEntry();
 	
-	public static void recordStartReading(Context context , String md5)
+	public static void recordStartReading(Context context, String md5, OnyxBookProgress bookProgress)
 	{
 		sHistoryEntry.setStartTime(new Date());
 		sHistoryEntry.setEndTime(new Date());
+		sHistoryEntry.setProgress(bookProgress);
 		sHistoryEntry.setMD5(md5);
 		OnyxCmsCenter.insertHistory(context, sHistoryEntry);
 		Log.d(Tag, "insert id is " + sHistoryEntry.getId());
 		Log.d(Tag, "insert MD5 is " + sHistoryEntry.getMD5());
 	}
 	
-	public static void recordFinishReading(Context context)
+	public static void recordFinishReading(Context context, OnyxBookProgress bookProgress)
 	{
 		sHistoryEntry.setEndTime(new Date());
+		sHistoryEntry.setProgress(bookProgress);
 		OnyxCmsCenter.updateHistory(context, sHistoryEntry);
 		Log.d(Tag, "update id is " + sHistoryEntry.getId());
 		Log.d(Tag, "update MD5 is " + sHistoryEntry.getMD5());
