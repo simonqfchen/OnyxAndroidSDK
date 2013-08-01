20511a724aa5a774f8dc4de27f674638c3ddfec0
joy <joy@onyx-international.com>
2013-08-01 11:58:18 +0800

    add infrastructure in com.onyx.android.sdk.reader to support IRemoteRenderService

--- src/com/onyx/android/sdk/data/util/BitmapUtil.java
+++ src/com/onyx/android/sdk/data/util/BitmapUtil.java
@@ -5,8 +5,13 @@ package com.onyx.android.sdk.data.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.MemoryFile;
+import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
 /**
@@ -17,10 +22,11 @@ public class BitmapUtil
 {
     private static final String TAG = "BitmapUtil";
     
-    public static byte[] getByteArrayFromBitmap(Bitmap bmp)
+    public static byte[] getByteArray(Bitmap bmp)
     {
         final ByteArrayOutputStream os = new ByteArrayOutputStream();
         try {
+            ProfileUtil.start(TAG, "compress bitmap");
             bmp.compress(Bitmap.CompressFormat.JPEG, 100, os);
             return os.toByteArray();
         }
@@ -31,6 +37,83 @@ public class BitmapUtil
             catch (IOException e) {
                 Log.e(TAG, "exception", e);
             }
+            ProfileUtil.end(TAG, "compress bitmap");
+        }
+    }
+    
+    /**
+     * never return null
+     * 
+     * @param bmp
+     * @return
+     */
+    public static int[] getPixels(Bitmap bmp)
+    {
+        final int width = bmp.getWidth();
+        final int height = bmp.getHeight();
+
+        int buf[] = new int[width * height];
+        bmp.getPixels(buf, 0, width, 0, 0, width, height);  
+        
+        return buf;
+    }
+    
+    /**
+     * never return null
+     * 
+     * @param bmp
+     * @return
+     */
+    public static byte[] getPixelsInByte(Bitmap bmp)
+    {
+        final int width = bmp.getWidth();
+        final int height = bmp.getHeight();
+        ByteBuffer byte_buf = ByteBuffer.allocate(width * height * 4);
+        bmp.copyPixelsToBuffer(byte_buf);
+        
+        return byte_buf.array();
+    }
+    
+    public static ParcelFileDescriptor createMemoryFile(Bitmap bmp)
+    {
+        MemoryFile mf = null;
+        try {
+            byte[] pixels = getByteArray(bmp);
+
+            mf = new MemoryFile(null, pixels.length);
+            mf.allowPurging(false);
+
+            ProfileUtil.start(TAG, "write MemoryFile");
+            OutputStream os = null;
+            try {
+                os = mf.getOutputStream();
+                os.write(pixels);
+            }
+            finally {
+                ProfileUtil.end(TAG, "write MemoryFile");
+                if (os != null) {
+                    os.close();
+                }
+            }
+        } catch (IOException e) {
+            Log.w(TAG, e);
+            if (mf != null) {
+                mf.close();
+            }
+            return null;
+        }
+        
+        return MemoryFileUtil.getParcelFileDescriptor(mf);
+    }
+    
+    public static Bitmap readMemoryFile(ParcelFileDescriptor pfd)
+    {
+        ProfileUtil.start(TAG, "readMemoryFile");
+        try {
+            return BitmapFactory.decodeFileDescriptor(pfd.getFileDescriptor());
+        }
+        finally {
+            ProfileUtil.end(TAG, "readMemoryFile");
         }
     }
 }
