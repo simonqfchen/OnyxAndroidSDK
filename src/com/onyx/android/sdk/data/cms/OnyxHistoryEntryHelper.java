1f8b205c7642fa4afbdb8aaf61c943d61e16e4ae
Joy <joy@onyx-international.com>
2013-08-27 11:30:38 +0800

    fix null pointer error in OnyxHistoryEntryHelper.recordFinishReading()

--- src/com/onyx/android/sdk/data/cms/OnyxHistoryEntryHelper.java
+++ src/com/onyx/android/sdk/data/cms/OnyxHistoryEntryHelper.java
@@ -17,25 +17,30 @@ public class OnyxHistoryEntryHelper
 {
 	private static final String Tag = "OnyxHistoryEntryHelper";
 	
-	private static OnyxHistoryEntry sHistoryEntry = new OnyxHistoryEntry();
+	private static OnyxHistoryEntry sHistoryEntry = null;
 	
 	public static void recordStartReading(Context context, String md5, OnyxBookProgress bookProgress)
 	{
+	    sHistoryEntry = new OnyxHistoryEntry();
+	    
 		sHistoryEntry.setStartTime(new Date());
 		sHistoryEntry.setEndTime(new Date());
 		sHistoryEntry.setProgress(bookProgress);
 		sHistoryEntry.setMD5(md5);
 		OnyxCmsCenter.insertHistory(context, sHistoryEntry);
-		Log.d(Tag, "insert id is " + sHistoryEntry.getId());
-		Log.d(Tag, "insert MD5 is " + sHistoryEntry.getMD5());
+		Log.d(Tag, "insert: " + sHistoryEntry.getId() + ", " + sHistoryEntry.getMD5());
 	}
 	
 	public static void recordFinishReading(Context context, OnyxBookProgress bookProgress)
 	{
+	    if (sHistoryEntry == null) {
+	        return;
+	    }
+
 		Date old_end_time = sHistoryEntry.getEndTime();
 		Date new_end_time = new Date();
-		int durationTime = 1;  //minute
-		if ((new_end_time.getTime() - old_end_time.getTime()) / 1000f >= durationTime * 60) {   // s
+		int durationTime = 10;  // minutes
+		if ((new_end_time.getTime() - old_end_time.getTime()) >= durationTime * 60 * 1000) {
 			Log.d(Tag, "Read idle for " + durationTime +" minutes timeout");
 			recordStartReading(context, sHistoryEntry.getMD5(), bookProgress);
 			return;
@@ -44,8 +49,7 @@ public class OnyxHistoryEntryHelper
 		sHistoryEntry.setEndTime(new_end_time);
 		sHistoryEntry.setProgress(bookProgress);
 		OnyxCmsCenter.updateHistory(context, sHistoryEntry);
-		Log.d(Tag, "update id is " + sHistoryEntry.getId());
-		Log.d(Tag, "update MD5 is " + sHistoryEntry.getMD5());
+		Log.d(Tag, "update: " + sHistoryEntry.getId() + ", " + sHistoryEntry.getMD5());
 	}
 	
 	public static List<OnyxHistoryEntry> getHistorysByMD5(Context context, String md5)
